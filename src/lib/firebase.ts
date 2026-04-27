b21fb50582ad643815bcd084eca381686b803ac0
gabrielhasahatan <gabrielsitompul82@gmail.com>
2026-04-28 00:33:12 +0700

    fix: api firebase (jan hack bg pliss)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, signInWithPopup, signOut } from 'firebase/auth'
 import { GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSu0VLVdWkD-MmJgRaxNj1et3Ilgx_HcE",
-  authDomain: "my-film-132a4.firebaseapp.com",
-  projectId: "my-film-132a4",
-  storageBucket: "my-film-132a4.firebasestorage.app",
-  messagingSenderId: "505335755056",
-  appId: "1:505335755056:web:e2ba089b037d898c2558f5",
-  measurementId: "G-QTP209B9DY"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGIN_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
