d408607098b9f218c2f753cdc6d43ddc23ba91f3
gabrielhasahatan <gabrielsitompul82@gmail.com>
2026-06-28 21:58:26 +0700

    fix: add Suspense for useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import AllTrendingDayList from "@/modules/AllTrending/components/all-trending-day-list"
 import ProviderSeriesHorizontal from "@/modules/ProviderSeriesList/components/provider-series-horizontal"
 import TvPopularList from "@/modules/TvPopular/components/tv-popular-list"
+import { Suspense } from "react"
 
 const page = () => {
   return (
     <>
-      <AllTrendingDayList />
-      <TvPopularList />
-      <ProviderSeriesHorizontal />
+      <Suspense>
+        <AllTrendingDayList />
+        <TvPopularList />
+        <ProviderSeriesHorizontal />
+      </Suspense>
     </>
   )
 }
